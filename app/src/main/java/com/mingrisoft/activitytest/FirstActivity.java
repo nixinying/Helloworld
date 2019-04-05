02c2fbdb7cc28618b5a9ff9adf3665519b34779a
nixinying <1006576773@qq.com>
2019-04-05 10:03:12 +0800

    Initial commit

---
+++ app/src/main/java/com/mingrisoft/activitytest/FirstActivity.java
@@ -0,0 +1,25 @@
+package com.mingrisoft.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_activity);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this,"信息已经发送",
+                    Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
